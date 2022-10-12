370d3d92e2dab3e66f5d9cfee867a377e763224c
twcrone <twcrone@gmail.com>
2022-10-12 00:14:44 -0400

    Get basic spring app with log4j going

--- src/main/java/com/latecurio/log4j1/Log4j1Application.java
+++ src/main/java/com/latecurio/log4j1/Log4j1Application.java
@@ -1,14 +1,20 @@
 package com.latecurio.log4j1;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Log4j1Application {
 
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
+	public static void main(String[] args) throws IOException {
+		//BasicConfigurator.configure();
+		ClassPathResource resource = new ClassPathResource("log4j.properties");
+		PropertyConfigurator.configure(resource.getInputStream());
 		SpringApplication.run(Log4j1Application.class, args);
 	}
 
--- src/main/java/com/latecurio/log4j1/LoggingController.java
+++ src/main/java/com/latecurio/log4j1/LoggingController.java
@@ -1,14 +1,18 @@
 package com.latecurio.log4j1;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class LoggingController {
 
-    private static Logger LOG = Logger.getLogger(LoggingController.class);
+    private static final Logger LOG = Logger.getLogger(LoggingController.class);
 
     @GetMapping("/info")
     public String logInfo(@RequestParam String message) {
